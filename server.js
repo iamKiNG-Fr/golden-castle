dea9852592c8c00c2a058d68203fc57103a4fb5a
King <francisking1st@gmail.com>
2023-06-06 22:08:19 +0100

    made construction page

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
+//middleware
 app.set('view engine', 'ejs')
+app.use(express.static("public"))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 
 //Routers location
 const guestRouters = require('./route/guests')
