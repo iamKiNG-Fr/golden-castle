3f3493578e0641f63e2a63a15785409e9d3cc240
King <francisking1st@gmail.com>
2023-07-12 10:51:48 +0100

    added ssl true to sequelize config

--- sequelize/config/config.js
+++ sequelize/config/config.js
@@ -20,6 +20,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    ssl: true
   }
 }
